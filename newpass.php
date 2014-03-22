1209ec7cf9ecbb95de8bfe2ede9775c420af487a
Dimitra P <themiszamani@gmail.com>
2014-03-22 17:59:56 +0200

    newpass with token

--- newpass.php
+++ newpass.php
@@ -1,3 +1,23 @@
+<?php 
+$newtoken = $_GET['token'];
+echo $newtoken;
+require_once('wp-config.php');
+
+$conn = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD) or die(mysql_error());
+mysql_set_charset('utf8',$conn);
+mysql_select_db(DB_NAME, $conn);
+$newtoken=html_entity_decode($newtoken);
+$query ="SELECT * FROM users WHERE _time='".$newtoken."'";
+echo $query;
+$result = mysql_query($query, $conn);
+$num_rows = mysql_num_rows($result);
+echo "$num_rows Rows\n";
+$exists=1;
+if ($num_rows>0) {
+	$exists=1;
+}
+
+if ($exists==1){?>
 <?php include("includes/header.php");?>
 <div class="row-fluid">&nbsp;
 	<div class="span4 offset4"><h3>Ξέχασα τον κωδικό μου</h3></div>
@@ -28,7 +48,8 @@
 							<button id="newpass" name="newpass" class="btn btn-primary btn-block" type="submit"> ΑΛΛΑΓΗ ΚΩΔΙΚΟΥ ( PASSWORD )</button>
 							</div>
 						</div>
-						<input type="hidden" id="email" name="email" value="<?php echo $_POST['email'];?>"/>
+						<input type="hidden" id="email" name="email" value="<?php echo $email;?>"/>
+						<input type="hidden" id="newtoken" name="newtoken" value="<?php echo $newtoken;?>"/>
 					</fieldset>
 
 				</form>
@@ -36,4 +57,9 @@
 		</div>
 	</div>
 </div>
+
 <?php include("includes/footer.php");?>
+<?php }else{
+	header('Location:resetemail-1.php?error=notoken');
+	exit;
+ }?>
